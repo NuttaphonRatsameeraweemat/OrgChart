93f2c48c17d1774cf3694416b89e4a9959ae4807
Nuttaphon Ratsameeraweemat <nuttaphon.rat@hotmail.com>
2019-04-05 09:25:30 +0700

    Update -Publish Profile -Fixed Swagger Endpoint Deploy to iis

--- OrgChart.API/Extensions/ServiceExtensions.cs
+++ OrgChart.API/Extensions/ServiceExtensions.cs
@@ -139,7 +139,7 @@ namespace OrgChart.API.Extensions
             // specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("../swagger/v1/swagger.json", "My API V1");
             });
         }
 
